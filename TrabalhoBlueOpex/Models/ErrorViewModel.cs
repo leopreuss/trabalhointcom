8833b53032168aa8857df2afaf9239f276cfd2d2
DESKTOP-29GDR08\leopr <leopreuss@gmail.com>
2019-01-24 21:00:00 -0200

    Adicionar arquivos de projeto.

---
+++ TrabalhoBlueOpex/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrabalhoBlueOpex.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
